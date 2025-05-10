81907cb3494242d45cd45f59fdaaedeefdc40889
Aleksandr802 <savs802@gmail.com>
2025-05-10 08:14:49 -0700

    Initial Commit

---
+++ Ringing Out/Ringing_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ringing_OutApp.swift
+//  Ringing Out
+//
+//  Created by Oleksandr Seminov on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Ringing_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
